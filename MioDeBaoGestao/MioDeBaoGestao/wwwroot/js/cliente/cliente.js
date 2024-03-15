85070127825d343de3c698c480f2288584bd1662
IgorTorres <igor.torres@trustprev.com.br>
2024-03-15 17:52:09 -0300

    Ajuste para publicação

--- MioDeBaoGestao/MioDeBaoGestao/wwwroot/js/cliente/cliente.js
+++ MioDeBaoGestao/MioDeBaoGestao/wwwroot/js/cliente/cliente.js
@@ -10,7 +10,18 @@
 
     let url = "/Cliente/Search?search=" + search + "&page=" + page;
 
+    url = normalizeUrl(url);
+
     location.href = url;
+}
+
+function normalizeUrl(url) {
+    let sub = $("#sub-url").val();
+
+    if (sub.length > 0)
+        url = sub + url;
+
+    return url;
 }
 
 function selecionarCliente(id, nome) {
@@ -21,9 +32,14 @@ function selecionarCliente(id, nome) {
     $("#modal-cliente").modal('hide');
 }
 
-function getClienteModal() {
+function getClienteModal(page) {
+    let search = $("#pesquisa-txt").val();
+
+    let url = "/Cliente/ListarClientePartial?search=" + search + "&page=" + page;
+    url = normalizeUrl(url);
+
     $.ajax({
-        url: '/Cliente/ListarClientePartial',
+        url: url,
         type: "GET",
         dataType: "html",
         success: function (response) {
@@ -32,9 +48,53 @@ function getClienteModal() {
         },
         error: function (e) {
             if (e.status === 400)
-                alert(e.responseText);
+                swal(e.responseText, { icon: "warning", title: "Alerta" });
             else
-                alert("Não foi possível completar a transação!");
+                swal("Não foi possível completar a transação!", { icon: "error", title: "Erro" });
+
+        }
+    }).done(function (results) {
+    });
+}
+
+function excluirCliente(id) {
+    swal({
+        title: "Exclusão do cliente",
+        text: "Confirma a exclusão do cliente?",
+        icon: "warning",
+        buttons: {
+            cancel: "Cancelar",
+            confirm: "Excluir",
+        }
+    })
+        .then((update) => {
+            if (update) {
+                let url = '/Cliente/Delete?id=' + id;
+                url = normalizeUrl(url);
+                ajaxGET(url);
+            }
+        });
+}
+
+function ajaxGET(url) {
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+            swal(response,
+                {
+                    icon: "success",
+                    buttons: {
+                        confirm: "OK",
+                    }
+                }).then((confirm) => { location.reload(); });
+        },
+        error: function (e) {
+            if (e.status === 400)
+                swal(e.responseText, { icon: "warning", title: "Alerta" });
+            else
+                swal("Não foi possível completar a transação!", { icon: "error", title: "Erro" });
 
         }
     }).done(function (results) {
